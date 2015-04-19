78ac1da6d4ac23e7e7100a2f6e4121e58e74b015
Kevin Zheng <kevin.zheng95@gmail.com>
2015-04-19 19:33:57 +1000

    733 pm 19042015 minor changes

--- myQuiz/www/js/dashboard.js
+++ myQuiz/www/js/dashboard.js
@@ -16,9 +16,10 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-
+$(document).ready(function(){
 $(".menu-opener").click(function(){
   $(".menu-opener, .menu-opener-inner, .menu").toggleClass("active");
+});
 });
 
 
